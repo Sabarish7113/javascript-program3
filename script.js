ab586f23c41ec029751b9efe62ead4b1c80b22d5
Sabarish7113 <sabarish.user@gmail.com>
2024-07-31 17:11:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+//merge two array
+// a=[1,2,3,4,5]
+// b=[6,7,8,9,10]
+// c=a+b
+// document.write(c)
+
+//spread operator
+// a=[6,7,8,9,10]
+// b=[1,2,3,4,5,...a]
+// document.write(b)
+
+
+//push
+// a=[1,2,3,4,5]
+// a.push(6)
+// document.write(a)
+
+//unshift
+// a=[1,2,3,4,5]
+// a.unshift(6,7)
+// document.write(a)
+
+//add array 
+// let gfg = [];
+// let row = 3;
+// let col = 3;
+// let h = 0
+// for (let i = 0; i < row; i++) {
+//     gfg[i] = [];
+//     for (let j = 0; j < col; j++) {
+//         gfg[i][j] = h++;
+//     }
+// }
+// console.log(gfg);
+
+
